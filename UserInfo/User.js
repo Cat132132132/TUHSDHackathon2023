30e39d171e0765a7bad160eae84dcbfd74a0762d
Zander Coffman <zandergalvez330@gmail.com>
2023-04-01 15:41:36 -0700

    Added the bar graph and the specific colors, but made a default value

--- UserInfo/User.js
+++ UserInfo/User.js
@@ -1,5 +1,6 @@
 // since there is only going to be one instence of this running
 // only one file is used to gather the data stored
+localStorage.setItem("a", "as");
 class User
 {
     constructor(name, weight, height, gender)
@@ -35,7 +36,5 @@ class User
     getGender()
     {
         return this.gender;
-    }
-
-    
+    }  
 }
