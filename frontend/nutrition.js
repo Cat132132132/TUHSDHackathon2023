7783262236ddb32590c9a20ba07673ec2cf5b37c
Daniel Briggs <dtbriggs123@gmail.com>
2023-04-01 16:16:03 -0700

    forms added

--- frontend/nutrition.js
+++ frontend/nutrition.js
@@ -1,3 +1,18 @@
 function add_food_visible() {
-    document.getElementById("add_food_div").style.visibility="visible"
+    document.getElementById("add_food_div").style.display="shown"
+    location.replace("#add_food_div")
+}
+//var button = document.getElementById("addFood");
+//button.addEventListener("click", updatePage);
+function getFood()
+{
+
+}
+function updateValues()
+{
+
+}
+function getAPIValues()
+{
+
 }
