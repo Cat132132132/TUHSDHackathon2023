490f6d6ccd4beb11e595b18e76aa243d163fca5d
Zander Coffman <zandergalvez330@gmail.com>
2023-04-01 16:22:48 -0700

    Added the button and input files

---
+++ frontend/nutritionPageCode.js
@@ -0,0 +1,19 @@
+var addFood_button = document.getElementById("addNutrients");
+var input1 = document.getElementById("input1");
+var input2 = document.getElementById("input2");
+var input3 = document.getElementById("input3");
+var input4 = document.getElementById("input4");
+var input5 = document.getElementById("input5");
+button.addEventListener("click", updatePage);
+function getFood()
+{
+
+}
+function updateValues()
+{
+
+}
+function getAPIValues()
+{
+
+}
