887a7621b92c3a430dcb632610d29edf182b3f29
DC <dariuscollins505@gmail.com>
2023-04-01 12:39:39 -0700

    added to User.js

--- UserInfo/User.js
+++ UserInfo/User.js
@@ -1,11 +1,47 @@
 //this class will be used for each person that will be logged in
 class User
 {
-    constructor(name, dietchoice, weight, height)
+    constructor(name, dietchoice, weight, height, gender)
     {
         this.name = name;
         this.dietchoice = dietchoice;
         this.weight = weight;
         this.height = height;
+        this.gender = gender;
     }
+
+    /**
+     * 
+     * @returns name of user
+     */
+    getName()
+    {
+        return this.name;
+    }
+
+    /**@returns diet choice */
+    getDietChoice()
+    {
+        return this.dietchoice;
+    }
+
+    /**@returns weight in lbs */
+    getWeight()
+    {
+        return this.weight;
+    }
+
+    /**@returns height in inches */
+    getHeight()
+    {
+        return this.height;
+    }
+
+    /**@returns gender as a string ('male' or 'female' only) */
+    getGender()
+    {
+        return this.gender;
+    }
+
+    
 }
