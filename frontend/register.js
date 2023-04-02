446c2a4b9b952e13ff443e0adff5f788a5b2e2c0
furioushedgehog <kjahedgehog2646@gmail.com>
2023-04-02 09:52:19 -0700

    changed register form

--- frontend/register.js
+++ frontend/register.js
@@ -8,7 +8,15 @@ document.getElementById("submit").addEventListener("click", function()
     var weight = document.getElementsByName("weight")[0].value;
     var height = document.getElementsByName("height")[0].value;
     var age = document.getElementsByName("age")[0].value;
-    var gender = document.getElementsByName("gender")[0].value;
+    if(document.getElementById('male').checked)
+    {
+        var gender = document.getElementById("male").value;
+    }
+    else if(document.getElementById('female').checked)
+    {
+        var gender = document.getElementById("female").value;
+    }
+    
     window.user = new User(name, weight, height, age, gender);
     console.log("Registered ",name,weight,height,age,gender)
     localStorage.setItem("name", name);
