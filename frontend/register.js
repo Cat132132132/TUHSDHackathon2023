0805dfabdb790bd4e4d42a9c92b98d1039e0a800
DC <dariuscollins505@gmail.com>
2023-04-01 21:21:21 -0700

    added localStorage functionality, added to User constructor

--- frontend/register.js
+++ frontend/register.js
@@ -9,7 +9,11 @@ document.getElementById("submit").addEventListener("click", function()
     var height = document.getElementsByName("height")[0].value;
     var age = document.getElementsByName("age")[0].value;
     var gender = document.getElementsByName("gender")[0].value;
-    user = new User(name, weight, height, gender);
-    console.log(name,weight,height,age,gender)
-    
+    user = new User(name, weight, height, age, gender);
+    console.log("Registered ",name,weight,height,age,gender)
+    localStorage.setItem("name", name);
+    localStorage.setItem("weight", weight);
+    localStorage.setItem("height", height);
+    localStorage.setItem("age", age);
+    localStorage.setItem("gender", gender);
 });
